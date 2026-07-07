e178f170ebe73de797f5d76803c81679818e0d33
Mouad Lasri <mouadkor20@gmail.com>
2026-07-07 15:02:38 +0900

    first commit

---
+++ src/main/java/org/practice/multitenanttaskmanagementapi/MultiTenantTaskManagementApiApplication.java
@@ -0,0 +1,13 @@
+package org.practice.multitenanttaskmanagementapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantTaskManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiTenantTaskManagementApiApplication.class, args);
+    }
+
+}
